f2074b0de270c8796bb7d1e3f0ccfb6cce7001c2
y4shiro <y4shironao@gmail.com>
2020-12-03 16:30:13 +0900

    1-07 JS の計算

---
+++ chapter1/js-grammar/calc.js
@@ -0,0 +1,21 @@
+// var x = 10;
+// var x = 'JavaScript の変数';
+// document.write(x);
+
+const before = 1000000;
+const mul = 1.06 ** 10;
+const after = before * mul;
+const answer = after - before;
+// document.write(answer);
+
+// このコードは三角形の面積を求めるプログラムです
+const w = 3; // 三角形の底辺
+const h = 10; // 三角形の高さ
+const area = (w * h) / 2; // 三角形の面積の公式を使って計算し、area 変数へ結果を代入
+// document.write(area); // DOM に面積を出力
+
+// 練習: 半径12cmの円の面積を出力
+const radius = 12;
+const pi = 3.14;
+const circle = radius ** 2 * pi;
+document.write(circle);
