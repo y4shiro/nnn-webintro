6d0eeafa1021d5dbf67d0efc64c48578e343cdd0
y4shiro <y4shironao@gmail.com>
2020-12-03 22:14:57 +0900

    1-14 CSS を使ったプログラミング

---
+++ chapter1/css-study/animation.js
@@ -0,0 +1,16 @@
+let header = document.getElementById('header');
+let degree = 0;
+
+function rotateHeader() {
+  degree += 6;
+  degree %= 360;
+  if ((0 <= degree && degree < 90) || (270 <= degree && degree < 360)) {
+    header.className = 'face';
+  } else {
+    header.className = 'back';
+  }
+  header.style.transform = 'rotateX(' + degree + 'deg)';
+}
+
+setInterval(rotateHeader, 20);
+
